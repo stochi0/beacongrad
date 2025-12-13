Fix gradient shape handling in Tensor class during backward propagation

src/tensor.py
@@ -55,7 +55,7 @@ class Tensor:
             if self.requires_grad:
                 self.grad += unbroadcast(other.data * self.data ** (other.data - 1) * out.grad, self.shape)
             if other.requires_grad:
-                other.grad += unbroadcast(self.data ** other.data * np.log(self.data) * out.grad, self.shape)
+                other.grad += unbroadcast(self.data ** other.data * np.log(self.data) * out.grad, other.shape)
         out._backward = _backward
         return out
 
@@ -95,7 +95,7 @@ class Tensor:
         build_topo(self)
         for node in topo:
             node.grad = np.zeros_like(node.data) if node.requires_grad else None
-            
+
         self.grad = np.ones_like(self.data) if self.requires_grad else None
         for node in reversed(topo):
             node._backward()